8c4af2b2daab42cc4660173e7cd8c6b95298e1a2
Robert Joyner <rjoyner@epix.net>
2022-06-23 00:41:05 -0400

    Initial Commit

---
+++ data_type.py
@@ -0,0 +1,40 @@
+#clear terminal
+import os
+os.system('clear')
+
+################################################
+
+#DATA TYPES
+#Strings
+#Number
+#Lists
+#Tuples
+#Booleans
+#Dictiionaries
+################################################
+
+#String type
+first_name = "Robert"
+#Number type
+age = "55"
+#Lists type
+names_numbers = ["John", "Kate", "George", 57]
+#Tuples is a list that you can't edit
+names = ("Ben", "Dick", "Henry")
+#Dictionary type
+fav_pizza = {
+	"Ben": "Pepperoni",
+	"Dick": "Mushroom",
+	"Henry": "Cheese"
+}
+#Boolean type is either True or False
+
+name_boolean = True
+
+print(first_name)
+print(age)
+print(names_numbers)
+print(names)
+print(fav_pizza["Dick"])
+print(name_boolean)
+
