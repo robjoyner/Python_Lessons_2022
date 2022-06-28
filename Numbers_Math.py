7fc34c08af665f688cfe8abb8276915b79029e0b
Robert Joyner <rjoyner@epix.net>
2022-06-27 22:58:29 -0400

    Adding Numbers and Math lesson

---
+++ Numbers_Math.py
@@ -0,0 +1,66 @@
+#clear terminal
+import os
+os.system('clear')
+
+
+# Integer is whole number ie 10, A Float is a decimal number ie 10.26
+
+num = 10
+
+# Integer output
+print(num)
+
+# float output
+print(float(num))
+
+num1 = 10.25
+
+#Integer output
+print(int(num1))
+
+# Math operation, addtion, subtraction, multiplication, division, etc
+
+#additon
+print(7+2)
+
+# subtraction
+
+print(7-2)
+
+#multiplication
+
+print(7*2)
+
+num_1 = 5
+num_2 = 2
+
+print(num_1 * num_2)
+
+#division
+
+print(7/2)
+
+#Powers ie X raised Y , X = 5, Y = 2
+
+print(5**2)
+
+# Modulus is the remainder after the division operation
+
+print(10%2)
+
+#or 
+
+print(10%3)
+
+# Order of opertations rule: 1.parentheses, 2.Exponents, 3.Multiply or Divide, 4.Add or Subtract
+
+print(4+1*3)
+
+print((4+1)*3)
+
+
+
+
+
+
+
